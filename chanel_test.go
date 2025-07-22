ffb1e534d116d7ef978eab5a1b50d0cc06f61856
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-07-22 21:42:09 +0700

    Add tests for channel parameter passing and in/out operations

--- chanel_test.go
+++ chanel_test.go
@@ -22,3 +22,40 @@ func TestCreateChan(t *testing.T) {
 	time.Sleep(5 * time.Second)
 
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Ilham Lii"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Ilham Lii"
+}
+
+func OnlyOut(channel chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+
+}
