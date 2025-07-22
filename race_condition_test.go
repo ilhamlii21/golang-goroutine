d194a2ffee43a499d89ebf82d4353540f71ad8e8
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-07-23 00:34:30 +0700

    Add mutex and race condition tests to validate concurrent access handling

---
+++ race_condition_test.go
@@ -0,0 +1,23 @@
+package golanggoroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestRaceCondition(t *testing.T) {
+
+	x := 0
+
+	for i := 1; i <= 100; i++ {
+		go func() {
+			for j := 1; j <= 1000; j++ {
+				x = x + 1
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter = ", x)
+}
