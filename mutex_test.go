2b038d13af11a45cdf594612fe857a3203a8f025
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-07-26 17:22:02 +0700

    Add tests for RWMutex to validate concurrent balance operations

--- mutex_test.go
+++ mutex_test.go
@@ -19,9 +19,46 @@ func TestMutex(t *testing.T) {
 				x = x + 1
 				mutex.Unlock()
 			}
+
 		}()
 	}
 
 	time.Sleep(5 * time.Second)
 	fmt.Println("Counter = ", x)
+}
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RWMutex.Unlock()
+
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.Balance
+	account.RWMutex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance", account.GetBalance())
 }
